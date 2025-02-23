b633d0c87616a0e643ee908eeed605884235926b
t3tsut3tsu <thevalya9@mail.ru>
2025-02-23 20:16:38 +0700

    add args, **kwargs; config file with test values; empty database

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import argparse
 import re
 
-print('Canary token v.0\n')
 # класс для обработки аргументов командной строки
 class Parse:
     def parser_args(self):
         parser = argparse.ArgumentParser(description="Takes the templates and sends them to the \
-        addresses. Then we'll start listening and waiting for the result until...", epilog="...trust is broken due to honeytoken.") #объект-обработчик аргументов ArgumentParser
-        parser.add_argument('mail_list', type=argparse.FileType('r'), help='Set the file with the email addresses') #позиционный арг., содержит путь к файлу с адресами
-        return parser.parse_args() #возвращает объект с доступом к значениям введенных пользователем аргументов
+        addresses. Then we'll start listening and waiting for the result until...", epilog="...trust is broken due to honey token.") # объект-обработчик аргументов ArgumentParser
+        parser.add_argument('mail_list', type=argparse.FileType('r'), help='set file with email addresses')# позиционный арг., содержит путь к файлу с адресами
+        parser.add_argument('--extension', choices=['docx', 'pdf', 'xlsx'], default='docx', help="set template's extension") # опциональный арг., содержит формат файла-шаблона для отправки
+        parser.add_argument('-p', '--ports', type=int, default=25, nargs='+' , help="set SMTP-port/'s for sending") # опциональный арг., выбор порта/ов для отправки письма по SMTP | пока нет обработки
+        parser.add_argument('--from', type=str, default='test@example', help='set sender email address') # опциональный арг., выбор почтового адреса отправителя | пока нет обработки
+        parser.add_argument('-s', '--server', type=str, default='smtp.example', help='set sender server address') # опциональный арг., выбор адреса почтового сервера отправителя | пока нет обработки
+        return parser.parse_args() # возвращает объект с доступом к значениям аргументов
 
 class Validate:
-    def __init__(self, mail_list): #инициализатор для файла со списком
-        self.mail_list = mail_list
-    def handle_file(self): #обработчик почт
-        #файл, переданный как аргумент
-        with self.mail_list as f:
-            emails = [email.strip() for email in f]
-        regex = re.compile(r'[A-Za-z0-9]+([._!$^*-]|[A-Za-z0-9])+@[A-Za-z0-9]+([._!$^*-]|[A-Za-z0-9])+[.]+[a-z]{2,}')
-        valid_emails = []
-        for email in emails:
-            if re.fullmatch(regex, email):
-                valid_emails.append(email)
-            else:
-                print(f"Invalid: {email}") #отладка
-        return valid_emails
+    def __init__(self, **kwargs): # инициализатор для аргументов
+        self.mail_list = kwargs.get('mail_list')
+        self.extension = kwargs.get('extension')
+    def handle_file(self): # обработчик почт
+        try:
+            with self.mail_list as f: # файл, переданный как аргумент
+                emails = [email.strip() for email in f]
+            regex = re.compile(r'[A-Za-z0-9]+([._!$^*-]|[A-Za-z0-9])+@[A-Za-z0-9]+([._!$^*-]|[A-Za-z0-9])+[.]+[a-z]{2,}')
+            valid_emails = []
+            for email in emails:
+                if re.fullmatch(regex, email):
+                    valid_emails.append(email)
+                else:
+                    print(f"Invalid: {email}") # отладка
+            return valid_emails
+        except FileNotFoundError:
+            print("No such file")
+        except IOError:
+            print("EoF Err")
+    def extension_identify(self):
+        if self.extension == 'docx':
+            return "\ndocx extension selected"
+
 
 args = Parse().parser_args()
-valid = Validate(args.mail_list)
-valid_emails = valid.handle_file()
-print(valid_emails)
+valid = Validate(mail_list=args.mail_list, extension=args.extension) # экземпляр класса Validate
+
+valid_mails = valid.handle_file()
+file_format = valid.extension_identify()
+
+print(valid_mails)
+print(file_format)
