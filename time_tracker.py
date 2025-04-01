5dc1372898040a5e8443606569b8a2bad86d0abc
t3tsut3tsu <thevalya9@mail.ru>
2025-04-01 14:00:43 +0700

    upd gitignore, listener is empty

--- time_tracker.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def execution_time(start_time, section_name):
-    end_time = time.perf_counter()
-    exec_time = end_time - start_time
-    print(f"{section_name}: {exec_time:.6f} sec")
-    return end_time
